4ee4bc757a1c872c416e8637b34de014b107c930
Mayowa <mayowadpastevans@yahoo.co.uk>
2024-07-07 23:18:54 +0100

    Complete lesson6 impl

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson6/CollaborativeFilter.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson6/CollaborativeFilter.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 
-@Component
+@Component("CF")
 public class CollaborativeFilter implements Filter {
     public String[] getRecommendations(String movie) {
         //logic of collaborative filter
--- src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson6/RecommenderImplementation.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson6/RecommenderImplementation.java
@@ -9,14 +9,14 @@ public class RecommenderImplementation {
     // use filter interface to select filter
     @Autowired
     @Qualifier("CBF")
-    private Filter contentBasedFilter;
+    private Filter filter;
 
    
 
     public String[] recommendMovies(String movie) {
         // print the name of interface implementation being used
-        System.out.println("Name of the filter in use: " + contentBasedFilter + "\n");
-        String[] results = contentBasedFilter.getRecommendations("Finding Dory");
+        System.out.println("Name of the filter in use: " + filter + "\n");
+        String[] results = filter.getRecommendations("Finding Dory");
         return results;
     }
 }
